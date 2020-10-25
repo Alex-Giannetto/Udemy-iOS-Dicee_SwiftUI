c57e29e501df0d9fb2a394c83502a903fc5b313e
Alexandre Giannetto <32279162+Alexg78bis@users.noreply.github.com>
2020-10-25 21:43:12 +0100

    Initial Commit

---
+++ Udemy-iOS-Dicee_SwiftUI/Udemy_iOS_Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Udemy_iOS_Dicee_SwiftUIApp.swift
+//  Udemy-iOS-Dicee_SwiftUI
+//
+//  Created by Alex on 25/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Udemy_iOS_Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
